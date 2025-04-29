c893bd5ec4960851fa13e158539aed34eb007023
Mahak <Mahakalaie1@gmail.com>
2025-04-30 00:08:17 +0330

    create Inventory class

---
+++ src/inventory/Inventory.java
@@ -0,0 +1,72 @@
+package inventory;
+
+import products.Product;
+
+import java.util.ArrayList;
+
+public class Inventory<T extends Product> {
+
+    private ArrayList<T> items;
+
+    public Inventory() {
+        items = new ArrayList<>();
+    }
+
+    public ArrayList<T> getItems() { return items; }
+
+    public void setItems(ArrayList<T> items) {
+        this.items.clear();
+        this.items.addAll(items);
+    }
+
+    public void applyDiscount(String productName, int discount) {
+
+        boolean notFound = true;
+
+        for (T t : items)
+        {
+            if( ! t.getTitle().equals(productName))
+                continue;
+
+            t.setPrice((double) (100 - discount) / 100 * t.getPrice());
+            notFound = false;
+        }
+
+        if(notFound)
+            throw new IllegalArgumentException("No product found with this name");
+    }
+
+    public void addItems( T product) {
+        items.add(product);
+    }
+
+    public void removeItemsById(int id) {
+
+        for(T t : items)
+        {
+            if(t.getId().equals(Integer.toString(id)))
+                items.remove(t);
+        }
+    }
+
+    public T findItemsById(int id) {
+
+        for(T t : items)
+        {
+            if( ! t.getId().equals(Integer.toString(id)))
+                continue;
+
+            return t;
+        }
+        System.out.println("no product found with this id");
+        return null;
+    }
+
+    public void displayAll() {
+
+        for(T t : items)
+        {
+            System.out.print(t.toString());
+        }
+    }
+}
