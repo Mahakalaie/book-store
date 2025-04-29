53730250119a89d23f98bab85937b4ca3c89dcbe
Mahak <Mahakalaie1@gmail.com>
2025-04-30 00:19:04 +0330

    create calculateTotalPrice method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import inventory.Inventory;
+import products.Product;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {}
+
+    public static double calculateTotalPrice(Inventory<? extends Product> inventory)
+    {
+        double sum = 0;
+
+        for(Product product : inventory.getItems())
+        {
+            sum += product.getPrice();
+        }
+
+        return sum;
     }
 }
